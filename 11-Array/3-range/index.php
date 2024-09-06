c562cec0d8808d7caf7d46c90e1174ef872076b5
RRD4RK <raphaeldegoes2007@gmail.com>
2024-09-06 16:53:25 -0300

    Aulas array

---
+++ 11-Array/3-range/index.php
@@ -0,0 +1,19 @@
+<?php
+//array de 1 a 10
+$arr = range(1, 10);
+
+print_r($arr);
+echo "\n";
+
+//array de 5 a 25
+$arr2 = range(5, 25);
+
+print_r($arr2);
+echo "\n";
+
+//array de 1 a 1000 com intervalo de 100 em 100
+
+$arr3 = range(0, 1000, 100);
+
+print_r($arr3);
+echo "\n";
---
+++ 11-Array/4-count/index.php
@@ -0,0 +1,16 @@
+<?php
+//count= fala quantos elementos tem dentro do array
+$arr = [1, 2, 3];
+
+echo count($arr);
+echo "\n";
+
+$arr2 = range(1, 200);
+
+echo count($arr2);
+echo "\n";
+
+$arr3 = ['nome' =>'Raphael', 'idade'=>17, 'profissão'=> 'dev'];
+
+echo count($arr3);
+
